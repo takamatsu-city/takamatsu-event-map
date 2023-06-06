1eb9d1b0a985e5482b819357a2c3c56fb5d80f60
naoki <n.globe.us@gmail.com>
2023-06-07 07:13:25 +0900

    add list switch condition

--- src/List.tsx
+++ src/List.tsx
@@ -1,20 +1,31 @@
+import { useRef } from 'react';
 import NewEvents from './ListContent/NewEvents';
 import MarkerDetail from './ListContent/MarkerDetail';
 import Search from './ListContent/Search';
 import SearchResults from './ListContent/SearchResults';
 
 const Content = (props: any) => {
-  // const {} = props;
-  
+  const { isPage } = props;
+
+  const listRef: React.MutableRefObject<HTMLDivElement| null> = useRef(null);
+
+  const listHandler = () => {
+    if (listRef.current && !listRef.current.classList.contains('open')) {
+      listRef.current.classList.add('open');
+    } else {
+      listRef.current?.classList.remove('open');
+    }
+  }
+
   return (
     <>
-      <div id="list">
+      <div id="list" ref={listRef} onClick={listHandler}>
         <label id="list-close"><span></span></label>
         <div id="list-content">
-          <NewEvents />
-          <MarkerDetail />
-          <Search />
-          <SearchResults />
+          {!isPage &&  <NewEvents />}
+          {isPage === 'marker' && <MarkerDetail />}
+          {isPage === 'search' && <Search />}
+          {isPage === 'searchResults' && <SearchResults />}
         </div>
       </div>
     </>
