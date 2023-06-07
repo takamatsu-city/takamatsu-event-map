8d48042475474c69454e9c537233ae8bf1974d4d
naoki <n.globe.us@gmail.com>
2023-06-07 14:19:18 +0900

    add popup

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import Header from './Header';
 import Banner from './Banner';
 import Map from './Map';
 import List from './List';
+import { Feature } from 'geojson';
 import './App.css';
 
 function App() {
   const [isPage, setIsPage] = useState<string | null>(null);
-  const listRef: React.MutableRefObject<HTMLDivElement| null> = useRef(null);
+  // TODO: GeoJSON の Features の型を指定する
+  const [events, setEvents] = useState<Feature[]>([]);
+  const listRef: React.MutableRefObject<HTMLDivElement | null> = useRef(null);
   
   return (
     <div className="App">
@@ -15,12 +18,14 @@ function App() {
       <Banner />
       <Map
         setIsPage={setIsPage}
+        setEvents={setEvents}
         listRef={listRef}
       />
       <List
         setIsPage={setIsPage}
         isPage={isPage}
         listRef={listRef}
+        events={events}
       />
     </div>
   );
--- src/List.tsx
+++ src/List.tsx
@@ -2,15 +2,17 @@ import NewEvents from './ListContent/NewEvents';
 import MarkerDetail from './ListContent/MarkerDetail';
 import Search from './ListContent/Search';
 import SearchResults from './ListContent/SearchResults';
+import { Feature } from 'geojson';
 
 type Props = {
   isPage: string | null;
   listRef: React.MutableRefObject<HTMLDivElement| null>;
+  events: Feature[],
   setIsPage: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
 const Content = (props: Props) => {
-  let { isPage, listRef, setIsPage } = props;
+  const { isPage, listRef, events, setIsPage } = props;
 
   const openListHandler = () => {
     if (listRef.current && !listRef.current.classList.contains('open')) {
@@ -32,7 +34,7 @@ const Content = (props: Props) => {
         <label id="list-close" onClick={closeListHandler}><span></span></label>
         <div id="list-content">
           {!isPage &&  <NewEvents />}
-          {isPage === 'marker' && <MarkerDetail />}
+          {isPage === 'marker' && <MarkerDetail events={events} />}
           {isPage === 'search' && <Search />}
           {isPage === 'searchResults' && <SearchResults />}
         </div>
