ad8b14a55ef4e6eb6ced9a29a86983d16385702a
naoki <n.globe.us@gmail.com>
2023-06-08 14:58:03 +0900

    fix date format

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,6 @@
 const formatDate = (date: string) => {
   const [year, month, day] = date.split('T')[0].split('-');
-  return `${day}.${month}.${year}`;
+  return `${year}.${month}.${day}`;
 }
 
 export default formatDate;
