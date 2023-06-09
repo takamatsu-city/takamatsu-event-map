24670648c1dfb854f5817b2d8e28aaef73638bbf
naoki <n.globe.us@gmail.com>
2023-06-09 21:48:50 +0900

    update style

--- src/Map.tsx
+++ src/Map.tsx
@@ -35,12 +35,11 @@ const Component = (props: Props) => {
 
     const map = new window.geolonia.Map({
       container: mapContainer.current,
-      zoom: 13.81,
-      center: [134.04937, 34.34965],
+      zoom: 15,
+      center: [134.04862, 34.35188],
       // @ts-ignore
       style: `${process.env.PUBLIC_URL}/style.json`,
       localIdeographFontFamily: 'sans-serif',
-      hash: true,
       minZoom: 11,
       maxZoom: 19,
     })
